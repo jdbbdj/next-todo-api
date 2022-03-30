18af0f7da87aa85b009871ece82fe0feb0d6c263
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-03-30 17:17:51 +0800

    Minor error handling

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,103 +4,95 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+Use Exception;
+Use Throwable;
 
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        $tasks = Task::all();
-        return response()->json([
-            'tasks'=>$tasks
-        ],201);
+    
+
+        try {
+            $result = Task::all();
+            return response()->json([
+                'tasks'=>$result
+            ],201);
+          
+          } catch (Throwable $e) {
+          
+            return response()->json([
+                'message'=>'Ye'
+            ],400);
+          }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $input = $request->all();
-        Task::create($input);
-        return response()->json([
-            'message'=>'created!'
-        ],201);
+        $result = Task::create($input);
+        if($result){
+            return response()->json([
+                'message'=>'User created successfully!',
+                'task'=>$result
+            ],201);
+        }else{
+            return response()->json([
+                'error'=>$result,
+                'message'=>'Something went wrong'
+            ],400);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        $task = Task::find($id);
-        return response()->json([
-            'task'=>$task
-        ],201);
+        $result = Task::find($id);
+        if($result){
+            return response()->json([
+                'task'=>$result
+            ],201);
+        }else{
+            return response()->json([
+                'error'=>$result,
+                'message'=>'Something went wrong'
+            ],400);
+        }
+
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request,$id)
     {
-        $contact = Task::find($id);
+        $result = Task::find($id);
+        if($result){
         $input = $request->all();
-        $contact->update($input);
+        $result->update($input);
         return response()->json([
             'message'=>'updated!'
         ],201);
 
+        }else{
+            return response()->json([
+                'message'=>'User not Found!'
+            ],400);
+        }
         
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        Task::destroy($id);
-        return response()->json([
-            'message'=>'deleted!'
-        ],201);
+        $result = Task::destroy($id);
+        if($result){
+            return response()->json([
+                'message'=>'deleted!'
+            ],201);
+        }else{
+            return response()->json([
+                'message'=>'Something went wrong!'
+            ],400);
+        }
     }
 }
